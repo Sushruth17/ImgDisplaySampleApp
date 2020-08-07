9e31d07e393ee96be80741cd1ec0d5065aff332f
Sushruth17 <sushruth.sushe@gmail.com>
2020-08-07 20:07:47 +0530

    enhancement

--- app/src/main/java/com/example/imgdisp/MainActivity.kt
+++ app/src/main/java/com/example/imgdisp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.imgdisp
 
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -10,13 +9,14 @@ import com.example.imgdisp.adapters.MyAdapter
 
 
 class MainActivity : AppCompatActivity() {
-    val imgUrl: ArrayList<String> = ArrayList()
+    private val imgUrl: ArrayList<String> = ArrayList()
     var recyclerView: RecyclerView? = null
     var manager: LinearLayoutManager? = null
     var adapter: MyAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-1_1.jpg")
         imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-2_1.jpg")
         imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-3_1.jpg")
@@ -26,67 +26,11 @@ class MainActivity : AppCompatActivity() {
         imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-7_0.jpg")
         imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-8_1.jpg")
         imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
+
         recyclerView = findViewById(R.id.recyclerView)
         manager = LinearLayoutManager(this)
         recyclerView?.layoutManager = manager
         adapter = MyAdapter(imgUrl, this)
         recyclerView?.adapter = adapter
-
-    }
-
-    private fun setData() {
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-1_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-2_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-3_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-4_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-5_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-6_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-7_0.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-8_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
-        imgUrl.add("https://gamedata.britishcouncil.org/sites/default/files/attachment/number-9_1.jpg")
     }
 }
--- app/src/main/java/com/example/imgdisp/adapters/MyAdapter.kt
+++ app/src/main/java/com/example/imgdisp/adapters/MyAdapter.kt
@@ -1,9 +1,7 @@
 package com.example.imgdisp.adapters
 
 
-import android.app.ProgressDialog
 import android.content.Context
-import android.content.Intent
 import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,12 +10,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.imgdisp.R
-import com.squareup.picasso.Picasso
-import java.security.AccessController.getContext
 
 
 class MyAdapter(var urls: ArrayList<String>,
@@ -25,39 +19,23 @@ class MyAdapter(var urls: ArrayList<String>,
     private var context = context_
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-         val image: ImageView = v.findViewById<ImageView>(R.id.img)
+         val image: ImageView = v.findViewById(R.id.img)
          val progress:ProgressBar = v.findViewById(R.id.progress_circular)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.listitem, parent, false)
-//        v.layoutParams = RecyclerView.LayoutParams(500, 250)
+        val v: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.listitem, parent, false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.i("binding", "binding for -->$position")
-        val circularProgressDrawable = CircularProgressDrawable(context)
-        circularProgressDrawable.strokeWidth = 5f
-        circularProgressDrawable.centerRadius = 30f
-        circularProgressDrawable.start()
-        if(position==8){
-//            holder.getImage().setImageDrawable(context.resources.getDrawable(R.drawable.ic_ds_buffer))
-            holder.image.visibility = View.INVISIBLE
-            holder.progress.visibility = View.VISIBLE
-            val imageView = holder.image
-            Handler().postDelayed({
-                holder.image.visibility = View.VISIBLE
-                holder.progress.visibility = View.INVISIBLE
-                if(position==8)
-                Glide.with(context)
-                    .load(urls[position])
-                    .into(imageView)
-                notifyItemRangeChanged(0,7)
-            }, 3000)
-        }
+        if(position==0)
+            delayItem(holder,0,3000)
+        else if(position==8)
+            delayItem(holder,8,6000)
         else{
             holder.image.visibility = View.VISIBLE
             holder.progress.visibility = View.INVISIBLE
@@ -69,6 +47,20 @@ class MyAdapter(var urls: ArrayList<String>,
 
     override fun getItemCount(): Int {
         return urls.size
+    }
+    
+    private fun delayItem(holder: ViewHolder,position: Int, delay: Long){
+        holder.image.visibility = View.INVISIBLE
+        holder.progress.visibility = View.VISIBLE
+        val imageView = holder.image
+        Handler().postDelayed({
+            holder.image.visibility = View.VISIBLE
+            holder.progress.visibility = View.INVISIBLE
+            Glide.with(context)
+                .load(urls[position])
+                .into(imageView)
+            notifyItemRangeChanged(1,7)
+        }, delay)
     }
 }
 
